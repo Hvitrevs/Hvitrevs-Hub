5c8e740cb6ac8f94556fdf3091e948bad4fad3ca
Hvitrevs <venusof12pm@proton.me>
2023-09-10 13:58:04 -0400

    building containers to reuse them across

--- src/styles/GlobalStyled.js
+++ src/styles/GlobalStyled.js
@@ -2,7 +2,21 @@ import styled from "styled-components";
 
 
 export const MainBody = styled.div`
-      background-color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
 
 
 `
+
+export const Container = styled.div`
+
+  width: 90%;
+  max-width: 1280px;
+  margin: auto;
+  
+  `
+
+  export const PaddingContainer = styled.div`
+  padding-top: ${({ top }) => top};
+  padding-bottom: ${({ bottom }) => bottom};
+  
+  `
