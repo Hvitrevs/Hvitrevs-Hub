3d27f3702ec42295257d38480c145a10aac47461
Hvitrevs <venusof12pm@proton.me>
2023-09-18 14:40:18 -0400

    added project objects to data file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Heading, LeText, PaddingContainer } from '../styles/GlobalStyled'
+import React from 'react';
+import { Heading, LeText, PaddingContainer } from '../styles/GlobalStyled';
+import { projectDetails } from '../utils/Data';
 
 const Projects = () => {
   return (
@@ -14,6 +15,9 @@ const Projects = () => {
       <Heading as='h1' size='h1' top='1rem' bottom='1rem'>
         Apps <LeText>I've built🛸 </LeText> 
       </Heading>
+      {projectDetails.map((project) =>(
+        <div>{project.project_name}</div>
+      ))}
     </PaddingContainer>
   ) 
 
--- src/utils/Data.js
+++ src/utils/Data.js
@@ -1,5 +1,7 @@
 import { FaReact, FaNode,} from 'react-icons/fa';
 import { SiExpress, SiMongodb, } from 'react-icons/si';
+import Desktop2 from '../assets/desktop2.png';
+import Desktop from '../assets/desktop.png';
 
 export const Skills = [
   {
@@ -29,3 +31,24 @@ export const Skills = [
 
 ];
 
+export const projectDetails = [
+  {
+    id: 0,
+    project_name: 'Weather Desktop Application',
+    project_desc: 'This is a weather API utilizer that transforms API data from open-metio.com into readable responsive webpage.',
+    tech_stack: ['Vite', 'Javascript', 'Tailwind' ],
+    project_img: Desktop2,
+    project_url: 'https://magical-concha-6ccb16.netlify.app',
+    reverse: false,
+  },
+  {
+    id: 1,
+    project_name: 'Admin Dashboard Application',
+    project_desc: 'Front-End sales dashboart utilizing recharts into responsive webpage. No server side, demo version only.',
+    tech_stack: ['Vite', 'TypeScript', 'SASS', 'React' ],
+    project_img: Desktop,
+    project_url: 'https://incandescent-vacherin-43b64e.netlify.app',
+    reverse: false,
+  },
+
+]
