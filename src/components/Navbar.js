f4ee68b796cae119db599a0d1dae323a6533f035
Hvitrevs <venusof12pm@proton.me>
2023-09-26 18:19:31 -0400

    adding navbar functions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { GiFox } from 'react-icons/gi'
-import { IconContainer } from '../styles/GlobalStyled'
+import { Container, Flexcontainer, IconContainer, PaddingContainer } from '../styles/GlobalStyled'
 import { theme } from '../utils/Theme'
+import { NavbarContainer } from '../styles/Navbar.styled'
 
 
 const Navbar = () => {
   return (
-    <></>
+    <NavbarContainer bgColor='transparent'>
+      <PaddingContainer top='1rem' bottom='1rem'>
+        <Container>
+          <Flexcontainer justify='space-between'>
+            
+
+          </Flexcontainer>
+        </Container>
+      </PaddingContainer>
+    </NavbarContainer>
   )
 }
 
--- src/styles/Navbar.styled.js
+++ src/styles/Navbar.styled.js
@@ -8,4 +8,11 @@ export const NavbarContainer = styled.div`
   z-index: 10;
   transition: all 0.3s ease-in;
   background-color: ${({ bgColor }) => bgColor};
+
+`
+
+export const Logo = styled.p`
+  font-size: 1rem;
+  font-weight: ${({ theme }) => theme.fonts.weight.medium};
+  color: white;
 `
