45fce98d276c24303f507afbabc7c3a850c39f46
Hvitrevs <venusof12pm@proton.me>
2023-09-26 18:02:38 -0400

    navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { GiFox } from 'react-icons/gi'
+import { IconContainer } from '../styles/GlobalStyled'
+import { theme } from '../utils/Theme'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <></>
   )
 }
 
