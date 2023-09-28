b3f25a43dfe9a997edea8de06071741c9c68dd50
Hvitrevs <venusof12pm@proton.me>
2023-09-28 16:49:06 -0400

    styled.div

--- src/components/layouts/NavMenu.js
+++ src/components/layouts/NavMenu.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { AiOutlineClose } from 'react-icons/ai';
-import { NavMenuContainer } from '../../styles/Navbar.styled';
-import { PaddingContainer } from '../../styles/GlobalStyled';
+import { MenuIcon, NavMenuContainer } from '../../styles/Navbar.styled';
+import { Flexcontainer, PaddingContainer } from '../../styles/GlobalStyled';
 
 const NavMenu = () => {
   return (
     <NavMenuContainer >
-      <PaddingContainer left='5%' right='5%'>
-
+      <PaddingContainer left='5%' right='5%' top='2rem'>
+        <Flexcontainer justify='flex-end'>
+          <MenuIcon>
+            <AiOutlineClose />
+          </MenuIcon>
+        </Flexcontainer>
       </PaddingContainer>
 
     </NavMenuContainer>
