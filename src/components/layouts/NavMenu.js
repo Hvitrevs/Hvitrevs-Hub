d07f0a95492f8d6f377288902c9db0fc11e129e4
Hvitrevs <venusof12pm@proton.me>
2023-10-07 12:20:45 -0400

    slider motion

--- src/components/layouts/NavMenu.js
+++ src/components/layouts/NavMenu.js
@@ -3,12 +3,13 @@ import { AiOutlineClose } from 'react-icons/ai';
 import { MenuIcon, MenuItem, NavMenuContainer } from '../../styles/Navbar.styled';
 import { Flexcontainer, PaddingContainer } from '../../styles/GlobalStyled';
 import { navLinks } from '../../utils/Data';
-
+import { motion } from 'framer-motion';
+import { slideInLeft } from '../../utils/Variants';
 const NavMenu = ({ setOpenMenu }) => {
   return (
 
     // close button for side menu
-    <NavMenuContainer >
+    <NavMenuContainer as={motion.div} variants={slideInLeft} initial='hidden' animate='visible' exit='exit'>
       <PaddingContainer left='5%' right='5%' top='2rem'>
         <Flexcontainer justify='flex-end' responsiveFlex>
           <MenuIcon size='2rem' onClick = {() => setOpenMenu(false)}
